b743dedafc7d56189f5d9f4c91b2fdd092c76ef1
ferran <ferran@192.168.1.131>
2020-04-06 18:19:21 +0200

    Day6: Get solution to repeated inserts. Implemented GET individual object from id and PUT method for update an object. RESTful API #100DaysOfCode

--- src/main/java/cat/emprul/model/dao/EventRaceDao.java
+++ src/main/java/cat/emprul/model/dao/EventRaceDao.java
@@ -26,6 +26,7 @@ public class EventRaceDao {
 	private static final String USER = "root";
 	private static final String PASSW = "";
 
+	//Constructors
 	private List<EventRace> listOfEventRace;
 
 	public EventRaceDao() {
@@ -37,6 +38,11 @@ public class EventRaceDao {
 		logger.fatal("We've just greeted the user!");
 	}
 
+	//Methods
+	/**
+	 * Method to get all the EventRace from database
+	 * @return
+	 */
 	public List<EventRace> getListOfEventRace() {
 		listOfEventRace = new ArrayList<EventRace>();
 
@@ -70,17 +76,62 @@ public class EventRaceDao {
 		} catch (SQLException e) {
 			logger.error("Error SQL" + e);
 			throw new EventRaceException(ErrorMessages.COULD_NOT_CONNECT_DATABASE.getErrorMessage());
-			
-//			.ErrorMessages.RECORD_ALREADY_EXISTS.getErrorMessage());
-
-
 		}
-		
 		
 		return listOfEventRace;
 	}
 	
+	/**
+	 * Method to get an EventRace from id_event from database
+	 * @return
+	 */
+	public EventRace getEventRacefromId(Long id_event) {
+		EventRace eventRace = null;
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+		} catch (ClassNotFoundException e) {
+			logger.error("Problem in loading MySQL JDBC driver");
+		}
+
+		try {
+			connection = DriverManager.getConnection(SERVERHOST, USER, PASSW);
+			
+			
+			String sqlSelectQuery = "SELECT id_event,nom,descripcio,lloc,pais,data_inici,data_fi FROM EVENTS " +
+						"WHERE id_event = ?";
+			
+			preparedStatement = connection.prepareStatement(sqlSelectQuery);
+
+			preparedStatement.setLong(1, id_event);
+			
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				eventRace = fillEventRacefromResulSet(resultSet);
+			}
+			
+			connection.close();
+		} catch (SQLException e) {
+			logger.error("Error SQL" + e);
+			throw new EventRaceException(ErrorMessages.COULD_NOT_CONNECT_DATABASE.getErrorMessage());
+		}
+		
+		return eventRace;
+	}
 	
+	
+	/**
+	 * Method to fill an object EventRace from tthe resulset
+	 * @param rs
+	 * @return
+	 * @throws SQLException
+	 */
 	private EventRace fillEventRacefromResulSet(ResultSet rs) throws SQLException {
 		EventRace eventRace = new EventRace();
 		eventRace.setId_event(rs.getLong("id_event"));
@@ -155,7 +206,7 @@ public class EventRaceDao {
 			
 			
 			String sqlInsertQuery = "UPDATE events SET " +
-					"nom = ? ,descripcio = ? ,lloc,pais = ? ,data_inici = ? ,data_fi = ? " +
+					"nom = ? ,descripcio = ? ,lloc = ? ,pais = ? ,data_inici = ? ,data_fi = ? " +
 					"WHERE id_event = ?";
 				
 			preparedStatement = connection.prepareStatement(sqlInsertQuery);
@@ -164,13 +215,12 @@ public class EventRaceDao {
 			preparedStatement.setString(2, eventRace.getDescription());
 			preparedStatement.setString(3, eventRace.getSite());
 			preparedStatement.setString(4, eventRace.getCountry());
-			preparedStatement.setDate(5, (java.sql.Date) eventRace.getDateStart());
-			preparedStatement.setDate(6, (java.sql.Date) eventRace.getDateEnd());
+			preparedStatement.setDate(5, new java.sql.Date(eventRace.getDateStart().getTime()));
+			preparedStatement.setDate(6, new java.sql.Date(eventRace.getDateEnd().getTime()));
 			
 			preparedStatement.setLong(7, eventRace.getId_event());
-			preparedStatement.executeUpdate(sqlInsertQuery);
+			preparedStatement.executeUpdate();
 			
-			connection.commit();
 			connection.close();
 		} catch (SQLException e) {
 			logger.error("Error SQL" + e);
@@ -178,5 +228,50 @@ public class EventRaceDao {
 			
 		}
 	}
+	
+	/**
+	 * Metode per comprovar si ja evistei una entrada amb el mateix nom
+	 */
+	public boolean testRepeatedEventRace(String name) {
+		boolean repeated = false;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+		} catch (ClassNotFoundException e) {
+			logger.error("Problem in loading MySQL JDBC driver");
+		}
+
+		try {
+			connection = DriverManager.getConnection(SERVERHOST, USER, PASSW);
+			
+			
+			String sqlSelectQuery = "SELECT nom FROM EVENTS " +
+						"WHERE nom = ?";
+			
+			preparedStatement = connection.prepareStatement(sqlSelectQuery);
+
+			preparedStatement.setString(1, name);
+			
+			resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				repeated = true;
+			}
+			
+			connection.close();
+		} catch (SQLException e) {
+			logger.error("Error SQL" + e);
+			throw new EventRaceException(ErrorMessages.COULD_NOT_CONNECT_DATABASE.getErrorMessage());
+		}
+		
+		return repeated;
+	}
+	
+	
 	
 }
--- src/main/java/cat/emprul/model/service/EventService.java
+++ src/main/java/cat/emprul/model/service/EventService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
@@ -47,7 +49,7 @@ public class EventService {
 	
 	
 	/**
-	 * This method return an XML with a list of events
+	 * This method return an JSON with a list of events
 	 * URL : http://localhost:8080/eventcal/rest/eventcal/events/listjson
 	 * @return
 	 */
@@ -62,16 +64,61 @@ public class EventService {
 		
 	}
 	
+	/**
+	 * This method return an JSON event with parameter {id_event} as @param id_event
+	 * URL : http://localhost:8080/eventcal/rest/eventcal/event/{id_event}
+	 * @param id_event
+	 * @return
+	 */
+	@GET
+	@Path("event/{id_event}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public EventRace getEventRacefromId(@PathParam("id_event") Long id_event) {
+		EventRaceDao eDao = new EventRaceDao();
+		return eDao.getEventRacefromId(id_event);
+	}
+	
+	/**
+	 * This method save an event
+	 * URL : http://localhost:8080/eventcal/rest/eventcal/events/post
+	 * Sample: {"country":"Catalunya","dateEnd":"1601933724","dateStart":"1601791200","description":"Cursa de la Ultra Pirineu","name":"Ultra Pirineu","site":"Bagà"}
+	 * @return
+	 */
 	@POST
 	@Path("events/post")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response insertEvenRace(EventRace eventRace) {
+		String result ="";
 		
 		EventRaceDao eDao = new EventRaceDao();
-		eDao.insertEvenRace(eventRace);
+		if ( eDao.testRepeatedEventRace(eventRace.getName())) {
+			result = "EventRace repeated";
+		} else {
+			eDao.insertEvenRace(eventRace);
+			result = "EventRace saved : " + eventRace;
+		}
 		
-		String result = "EventRace saved : " + eventRace;
 		return Response.status(201).entity(result).build();
 		
 	}
+
+	/**
+	 * This method update an event
+	 * URL : http://localhost:8080/eventcal/rest/eventcal/events/put
+	 * Sample: {"country":"Catalunya","dateEnd":"1601933724","dateStart":"1601791200","description":"Cursa de la Ultra Pirineu","name":"Ultra Pirineu","site":"Bagà"}
+	 * @return
+	 */
+	@PUT
+	@Path("events/put")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updatetEvenRace(EventRace eventRace) {
+		
+		EventRaceDao eDao = new EventRaceDao();
+		eDao.updatetEvenRace(eventRace);
+		
+		String result = "EventRace updated : " + eventRace;
+		return Response.status(201).entity(result).build();
+		
+	}
+	
 }
